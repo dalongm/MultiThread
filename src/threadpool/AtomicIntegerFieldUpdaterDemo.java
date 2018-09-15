df42c68cc971e0e90e0bdf25b1b55c6cca789703
dalongm <dalongmeng@outlook.com>
2018-09-15 11:23:40 +0800

    update

---
+++ src/threadpool/AtomicIntegerFieldUpdaterDemo.java
@@ -0,0 +1,36 @@
+package threadpool;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+public class AtomicIntegerFieldUpdaterDemo {
+    public static class Candidate{
+        int id;
+        volatile int score;
+    }
+
+    public final static AtomicIntegerFieldUpdater<Candidate> scoreUpdater=
+            AtomicIntegerFieldUpdater.newUpdater(Candidate.class,"score");
+    public static AtomicInteger allScore = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        final Candidate stu = new Candidate();
+        Thread[] ts = new Thread[10000];
+        for (int i = 0; i < 10000; i++) {
+            ts[i] = new Thread(){
+              public void run(){
+                  if(Math.random()>0.4){
+                      scoreUpdater.incrementAndGet(stu);
+                      allScore.incrementAndGet();
+                  }
+              }
+            };
+            ts[i].start();
+        }
+        for (int i = 0; i < 10000; i++) {
+            ts[i].join();
+        }
+        System.out.println("score="+stu.score);
+        System.out.println("allScore="+allScore);
+    }
+}
